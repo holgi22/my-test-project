040729cc03f8eac9057ab0cb9c334e554030135f
holgi22 <hobrgr@yahoo.de>
2020-07-01 08:36:27 +0200

    Änderung canvas

--- Assets/scripts/cuberotate.cs
+++ Assets/scripts/cuberotate.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class cuberotate : MonoBehaviour
 {
+    
+    
+    
+    
     //Rotational Speed
     public float speed = 0f;
 
@@ -17,12 +21,57 @@ public class cuberotate : MonoBehaviour
     public bool ReverseY = false;
     public bool ReverseZ = false;
 
+
+    public float Geschwindigkeit = 0f;
+    public Vector3 Richtung = new Vector3 (0, 0, 0);
+    float degrees = 90;
+
+
+
+
+
     void Update()
     {
+        if (Input.GetKey(KeyCode.N))
+        {
+            ForwardX = false;
+            ForwardY = false;
+            // ForwardZ = false;
+            speed = 0f; ;
+            Vector3 to = new Vector3(0, 90, 0);
+            transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, to, Time.deltaTime);
+           
+        }
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            // ForwardX = true;
+            ForwardY = true;
+            // ForwardZ = true;
+            speed = 20f;
+        }
+
+        if (Input.GetKey(KeyCode.G))
+        {
+            Geschwindigkeit = 0f;
+            Richtung = new Vector3(0f, 90f, 0f);
+            this.transform.position = new Vector3(0, 0, 0);
+        }
+
+
+        if (Input.GetKey(KeyCode.L))
+        {
+            Geschwindigkeit = 10f;
+            Richtung = new Vector3(0f, 0f, 1f);
+            this.transform.position += Richtung * Geschwindigkeit * Time.deltaTime;
+        }
+
+        this.transform.position += Richtung * Geschwindigkeit * Time.deltaTime;
+
         //Forward Direction
         if (ForwardX == true)
         {
-            transform.Rotate(Time.deltaTime * speed, 0, 0, Space.Self);
+            // transform.Rotate(Time.deltaTime * speed, 0, 0, Space.Self);
         }
         if (ForwardY == true)
         {
@@ -30,7 +79,7 @@ public class cuberotate : MonoBehaviour
         }
         if (ForwardZ == true)
         {
-            transform.Rotate(0, 0, Time.deltaTime * speed, Space.Self);
+            // transform.Rotate(0, 0, Time.deltaTime * speed, Space.Self);
         }
         //Reverse Direction
         if (ReverseX == true)
